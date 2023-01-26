41c9589cc46f66d0344a3486721ce3b23e0a8835
hido02 <bestdohee@gmail.com>
2023-01-26 23:04:09 +0900

    [DP] 막대기 자르기

---
+++ 2022-2-java/src/dp/rodcut.java
@@ -0,0 +1,37 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class rodcut {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] partPrice = new int[n + 1];
+        int[] maxPricePerLength = new int[n + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 1; i <= n; i++) {
+            partPrice[i] = Integer.parseInt(st.nextToken());
+        }
+
+        br.close();
+
+        for (int i = 1; i <= n; i++) {
+            int max = 0;
+            for (int j = 1; j <= i; j++) {
+                max = Math.max(max, partPrice[j] + maxPricePerLength[i-j]);
+            }
+            maxPricePerLength[i] = max;
+        }
+
+        System.out.println(maxPricePerLength[n]);
+    }
+
+}
