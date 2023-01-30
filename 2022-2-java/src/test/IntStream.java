9f3514f233d4c90482e87eef04769df190fe8cf5
hido02 <bestdohee@gmail.com>
2023-01-30 23:10:11 +0900

    IntStream

---
+++ 2022-2-java/src/test/IntStream.java
@@ -0,0 +1,13 @@
+package test;
+
+public class IntStream {
+    public void for_loop() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+//    public void intStream_range() {
+//        IntStream.range(1, 11).forEach(System.out::println);
+//    }
+}
