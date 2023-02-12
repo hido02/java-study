3f0557e493892a6ed136d2ab29e646f835b5ce91
hido02 <bestdohee@gmail.com>
2023-02-12 20:11:07 +0900

    [Greedy] 프로그래머스: 체육복 (완)

--- 2022-2-java/src/Greedy/Physical.java
+++ 2022-2-java/src/Greedy/Physical.java
@@ -4,24 +4,29 @@ public class Physical {
     public static int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
 
-        int[] clothes = new int[n];
-
+        // 여벌 체육복을 가져온 학생이 도난당한 경우
         for(int i = 0; i < lost.length; i++) {
             for(int j = 0; j < reserve.length; j++) {
-                if(lost[i] == reserve[j]) { // 여벌 체육복을 가져온 학생이 도난당한 경우
-                    clothes[i] = 1;
-                }
-                clothes[i] = lost[i];
-                if(reserve[i] == i+1) {
-                    clothes[i] = 2;
+                if(lost[i] == reserve[j]) {
+                    answer++; // 여벌 체육복을 가져온 학생이 도난당했으므로 1개의 체육복을 가짐 -> 무조건 체육수업 들을 수 있음
+                    // lost, reserve 배열에서 제외시킨다
+                    lost[i] = -1;
+                    reserve[i] = -1;
+                    break;
                 }
             }
         }
 
-        for(int i = 0; i < n; i++) {
-            System.out.println(clothes[i]);
+        // 여벌 체육복이 없고 도난당한 경우
+        for(int i = 0; i < lost.length; i++) {
+            for(int j = 0; j < reserve.length; j++) {
+                if((lost[i]-1 == reserve[j]) || (lost[i]+1 == reserve[j])) { // 앞뒤로 빌려줄 수 있는 경우
+                    answer++;
+                    reserve[j] = -1; // 빌려줬으므로 reserve 배열에서 제외시킨다.
+                    break;
+                }
+            }
         }
-        System.out.println();
 
         return answer;
     }
