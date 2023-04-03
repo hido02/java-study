cfbd8f81873fb7a41a961e9204b964058eccb706
hido02 <bestdohee@gmail.com>
2023-04-03 20:57:24 +0900

    [이진탐색] 프로그래머스: 입국심사

--- 2022-2-java/src/Immigration/Solution.java
+++ 2022-2-java/src/Immigration/Solution.java
@@ -4,42 +4,32 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
-        Arrays.sort(times);
-        return BinarySearch(times,n,times[times.length - 1]);
-    }
+        long answer = 0;
 
-    long BinarySearch(int[] times, int n, long max){
-        long left = 1, right = max * n; // 최소 1분부터 가장 오래 걸리는 입국 심사 시간(times[times.length-1], 예시에서 10분)까지 탐색
-        long mid = 0;
-        long ans = Long.MAX_VALUE; // 가장 큰 값을 사용한 이유는 후에 바뀔 수도 있어서
+        Arrays.sort(times); // 가장 오래 걸리는 시간을 알아내기 위해 정렬
 
-        while (left <= right){ // 이진탐색
-            mid = (left+right)/2;
+        long start = times[0]; // 사람이 1명일 때인 times[0]을 대입
+        long end = (long)times[times.length-1]*(long)n; // 최대 시간 = 가장 오래 걸리는 시간 * 인원수
 
-            if (isPassed(times,n,mid)){ // isPassed: mid 시간 동안 n명이 심사받을 수 있는지 확인
-                ans = ans > mid ? mid : ans;
-                right = mid - 1;
-            }else{
-                left = mid + 1;
+        long mid;
+        long sum = 0;
+
+        while(start <= end) { // 이진탐색
+            mid = (start + end) / 2;
+            sum = 0; // 시간이 mid일 때 심사관들이 심사해준 사람들의 합
+            for(int time : times) {
+                sum += mid / time; // 심사관마다 mid를 기준으로 심사할 수 있는 사람의 수의 합 (내가 모르는 부분: 왜 mid를 time으로 나누는지,,?)
+            }
+
+            if(sum >= n) { // 심사하는 사람의 수가 더 많거나 같으므로 시간을 줄일 수 있다
+                answer = Math.min(answer, mid);
+                end = mid - 1; // 범위 줄이기
+            }
+            else {
+                start = mid + 1; // 범위 늘리기
             }
         }
-        return ans;
-    }
 
-    boolean isPassed(int[] times, int n, long mid){
-        long amount = 0; // 심사를 받은 사람 수
-
-        for(int i = 0; i < times.length; ++i){
-            amount += mid / times[i]; // mid값을 times로 나눠준다
-            // 예시
-            // mid: 30분 times[i]: 7분
-            // 30분 안에 7분이 몇번 수행될 수 있는지
-            // 30분 (7, 14, 21, 28) -> amount: 4
-
-            // mid: 30분 times[i]: 7분 n: 10명이라면
-            // 7-1명 14-1명 21-
-        }
-        if (amount>=n) return true;
-        else return false;
+        return answer;
     }
 }
